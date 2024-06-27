b7956609a3700e211be5557bca8264d8a9d43c8f
husknet <127146284+husknet@users.noreply.github.com>
2024-06-27 15:23:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
-// Initialize Web3Modal
-const providerOptions = {
-  walletconnect: {
-    package: window.WalletConnectProvider.default,
-    options: {
-      infuraId: "c05e035e823a4769b62ae15c1cbe2f02",
-    },
-  },
-};
+// app.js
 
-const web3Modal = new window.Web3Modal.default({
-  cacheProvider: false,
-  providerOptions,
+const connectButton = document.getElementById('connectButton');
+const accountInfo = document.getElementById('accountInfo');
+
+// Initialize Web3Modal
+const web3Modal = new Web3Modal.default({
+  cacheProvider: false, // optional
+  providerOptions: {} // required
 });
 
 let provider;
 let signer;
 
-const connectButton = document.getElementById('connectButton');
-const accountInfo = document.getElementById('accountInfo');
-
 async function connectWallet() {
   try {
-    if (window.ethereum) {
-      provider = new ethers.providers.Web3Provider(window.ethereum);
-      await provider.send('eth_requestAccounts', []);
-    } else {
-      provider = new ethers.providers.Web3Provider(await web3Modal.connect());
-    }
-
+    provider = new ethers.providers.Web3Provider(await web3Modal.connect());
     signer = provider.getSigner();
     const account = await signer.getAddress();
     accountInfo.innerText = `Connected account: ${account}`;
@@ -46,3 +33,4 @@ async function connectWallet() {
 }
 
 connectButton.addEventListener('click', connectWallet);
+
